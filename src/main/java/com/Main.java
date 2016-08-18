7e8a1026ad86c2ed5e0b33253c9bcfc49e1a1a5a
Jerry Yang <jerry@qoo-app.com>
2016-08-18 19:30:58 +0800

    init

---
+++ src/main/java/com/Main.java
@@ -0,0 +1,16 @@
+package com;
+
+/*import okhttp3.mockwebserver.MockWebServer;*/
+
+import java.io.IOException;
+
+/**
+ * Created by jerry on 2016/8/17.
+ */
+public class Main {
+    public static void main(String[] args) throws IOException {
+        //MockServerHelper.startMockServer();
+       /* MockWebServer server = new MockWebServer();
+        server.start(8080);*/
+    }
+}
